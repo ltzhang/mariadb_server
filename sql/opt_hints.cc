MariaDB/server

MDEV-36125 [NO_]INDEX_MERGE Hint
Introduces NO_INDEX_MERGE and INDEX_MERGE, which control whether or
not index merge strategies are used during query optimization.  Here
is an example query from the tests:

  SET optimizer_switch='index_merge_intersection=off';
  EXPLAIN SELECT /*+ INDEX_MERGE(t1 f4, f2) */ COUNT(*) FROM t1
  WHERE f4 = 'h' AND f2 = 2;

with the hint in place, the query plan will employ the index_merge
intersect strategy (abbreviated EXPLAIN output):

  type		Extra
  index_merge	Using intersect(f2,f4); Using where; Using index

The presence of the [NO_]INDEX_MERGE hint overrides the optimizer's
choice of keys during the index merge optimization.  As we see in
the above example, keys f2 and f4 and given and the optimizer will
consider only those keys for this query.

When the hint is given without any particular keys, as in
INDEX_MERGE(table), then all keys are considered.  In this case, the
cheapest index merge among the keys should be used.  When
NO_INDEX_MERGE(table) is given, then index merge is disabled for
that table.

When the hint is given with one or more keys, then only those keys
are considered.  In the case of NO_INDEX_MERGE, those keys are
excluded.  This can lead to no merged indexes at all, because
there may not be sufficient row-ordered read columns available for
consideration.

The index merge strategies of intersection, union, and sort union
cannot themselves be directly controlled via the hints.  In combination
with the optimizer switches for the same, the strategy may be
indirectly controlled but this is not guaranteed.

When the hint directs the optimizer such that insufficient ROR scans
are available, thus leading to a situation where the INDEX_MERGE hint
cannot be honored, the server will emit a warning to that effect.

In the hints module (opt_hints*{cc,h}), this commit adds some
index merge-specific functionality to make interpreting hint state
at callsites in the optimizer cleaner and more intuitive.
Particularly, we add a b

sql/opt_hints.cc
@@ -48,6 +48,7 @@ struct st_opt_hint_info opt_hint_info[]=
   {{STRING_WITH_LEN("DERIVED_CONDITION_PUSHDOWN")}, false, false, false},
   {{STRING_WITH_LEN("MERGE")}, false, false, false},
   {{STRING_WITH_LEN("SPLIT_MATERIALIZED")}, false, false, false},
+  {{STRING_WITH_LEN("INDEX_MERGE")}, false, false, false},
   {null_clex_str, 0, 0, 0}
 };
 
@@ -540,9 +541,7 @@ bool Opt_hints_table::fix_hint(TABLE *table)
     {
       if (key_info->name.streq((*hint)->get_name()))
       {
-        (*hint)->set_fixed();
-        keyinfo_array[j]= static_cast<Opt_hints_key *>(*hint);
-        incr_fully_fixed_children();
+        set_index_hint(*hint, j);
         break;
       }
     }
@@ -552,6 +551,24 @@ bool Opt_hints_table::fix_hint(TABLE *table)
     return false;
 
   return true; // Some children are not fully fixed
+}
+
+
+void Opt_hints_table::set_index_hint(Opt_hints *hint, uint arg)
+{
+  hint->set_fixed();
+  keyinfo_array[arg]= static_cast<Opt_hints_key *>(hint);
+  incr_fully_fixed_children();
+
+  /*
+    Update the index_merge_map to note that the key
+    is referenced by a [NO_]INDEX_HINT associated with
+    the query.
+  */
+  if (hint->is_specified(INDEX_MERGE_HINT_ENUM))
+    index_merge_map.set_key(arg);
+
+  // In the future, other hint types can be managed here.
 }
 
 
@@ -601,10 +618,136 @@ static bool get_hint_state(Opt_hints *hint,
 }
 
 
-/* 
+/*
+  Inspects the table and corresponding index_merge_map to
+  interpret index merge hint state.
+*/
+static void index_merge_hint_impl(const TABLE *table,
+                                  uint keyno,
+                                  bool &has_key_hint,
+                                  bool &other_key_hint,
+                                  bool &has_table_hint,
+                                  bool &hint_value)
+{
+  Opt_hints_table *table_hints= table->pos_in_table_list->opt_hints_table;
+  has_key_hint= false;
+  other_key_hint= false;
+  has_table_hint= false;
+  hint_value= false;
+
+  // Parent should always be initialized
+  if (!table_hints || keyno == MAX_KEY)
+    return;
+
+  const opt_hints_enum type_arg= INDEX_MERGE_HINT_ENUM;
+
+  // Get the hint state for the specific key, if named.
+  if (table_hints->keyinfo_array.size() > 0 &&
+      table_hints->keyinfo_array[keyno] &&
+      table_hints->keyinfo_array[keyno]->is_specified(type_arg))
+  {
+    has_key_hint= true;
+    hint_value= table_hints->keyinfo_array[keyno]->get_switch(type_arg);
+    return;
+  }
+
+  /*
+    The passed keyno doesn't have the hint specified, but see if another
+    has the [NO_]INDEX_MERGE hint specified.  If not, then see if the table
+    as a whole has the hint specified (implying all keys are affected).
+    There can't be a mix of NO_INDEX_MERGE and INDEX_MERGE hints for the
+    same table, so inspecting the first other specified key is enough.
+  */
+  const uint other_keyno= table_hints->index_merge_map.get_first_keyno();
+  if (table_hints->index_merge_map.has_key_specified() &&
+      table_hints->keyinfo_array[other_keyno] &&
+      table_hints->keyinfo_array[other_keyno]->is_specified(type_arg))
+  {
+    other_key_hint= true;
+    hint_value= table_hints->keyinfo_array[other_keyno]->get_switch(type_arg);
+    return;
+  }
+
+  // No specific key named, see if the table has the hint specified.
+  if (table_hints->is_specified(type_arg))
+  {
+    has_table_hint= true;
+    hint_value= table_hints->get_switch(type_arg);
+  }
+}
+
+
+// See comment in header file.
+index_merge_behavior index_merge_hint(const TABLE *table,
+                                     uint keyno,
+                                     bool &force_index_merge,
+                                     bool &use_cheapest_index_merge)
+{
+  bool has_key_hint= false,
+      other_has_hint= false,
+      has_table_hint= false,
+      hint_value= false;
+
+  index_merge_hint_impl(table,
+                        keyno,
+                        has_key_hint,
+                        other_has_hint,
+                        has_table_hint,
+                        hint_value);
+
+  if (has_key_hint && hint_value)
+  {
+    // Index merge is allowed for this key, so use it.
+    force_index_merge= true;
+    return index_merge_behavior::USE_KEY;
+  }
+
+  if (other_has_hint && hint_value)
+    // keyno isn't the one with the hint, another key on the table has the hint.
+    return index_merge_behavior::SKIP_KEY;
+
+  if (has_key_hint && !hint_value)
+    // This key is not allowed, so skip it.
+    return index_merge_behavior::SKIP_KEY;
+
+  if (other_has_hint && !hint_value)
+    // Another key is disallowed by the hint, this key is allowed.
+    return index_merge_behavior::USE_KEY;
+
+  if (has_table_hint && hint_value)
+  {
+    // No specific keys mentioned in the hint, so all are implied for the table.
+    force_index_merge= true;
+    use_cheapest_index_merge= true;
+    return index_merge_behavior::TABLE_ENABLED;
+  }
+
+  if (has_table_hint && !hint_value)
+    // Merging is disabled for all keys on the table.
+    return index_merge_behavior::TABLE_DISABLED;
+
+  // No hint specified for the table.
+  return index_merge_behavior::NO_HINT;
+}
+
+
+// See comment in header file.
+index_merge_behavior index_merge_hint(const TABLE *table,
+                                      uint keyno)
+{
+    bool force_index_merge_IGNORED= false,
+        use_cheapest_index_merge_IGNORED= false;
+    return index_merge_hint(table,
+                            keyno,
+                            force_index_merge_IGNORED,
+                            use_cheapest_index_merge_IGNORED);
+}
+
+
+/*
   @brief
     Check whether a given optimization is enabled for table.keyno.
-  
+
   @detail
     First check if a hint is present, then check optimizer_switch
 */

sql/opt_hints_parser.cc
@@ -119,6 +119,13 @@ Optimizer_hint_tokenizer::find_keyword(const LEX_CSTRING &str)
       return TokenID::keyword_JOIN_PREFIX;
     else if ("JOIN_SUFFIX"_Lex_ident_column.streq(str))
       return TokenID::keyword_JOIN_SUFFIX;
+    else if ("INDEX_MERGE"_Lex_ident_column.streq(str))
+      return TokenID::keyword_INDEX_MERGE;
+    break;
+
+  case 14:
+    if ("NO_INDEX_MERGE"_Lex_ident_column.streq(str))
+      return TokenID::keyword_NO_INDEX_MERGE;
     break;
 
   case 15:
@@ -466,6 +473,22 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
   opt_hints_enum hint_type;
   bool hint_state; // ON or OFF
 
+  /*
+    Intended for the [NO_]INDEX_MERGE hints originally, this is set
+    true when the hint should be set also on the table as well as
+    the index.  However, for such a hint set on the table, the check_parent flag
+    is inverted from this value; that is, if set_also_on_table is
+    true, then check_parent will be false (see below).  This is
+    because the presence of the hint on the table is treated differently
+    than the hint applying to all keys on the table.  For example,
+    INDEX_MERGE(table) means that the hint applies to all keys while
+    INDEX_MERGE(table, k1) means that the hint applies to key k1.  In
+    both cases, however, we set the hint both as an index-level hint and
+    as a table-level hint, but when inspecting it during index_merge_hint,
+    it is given special treatment (see that function for details).
+  */
+  bool set_also_on_table= false;
+
   switch (index_level_hint_type.id())
   {
   case TokenID::keyword_NO_ICP:
@@ -483,6 +506,16 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
   case TokenID::keyword_NO_RANGE_OPTIMIZATION:
     hint_type= NO_RANGE_HINT_ENUM;
     hint_state= true;
+    break;
+  case TokenID::keyword_INDEX_MERGE:
+    hint_type= INDEX_MERGE_HINT_ENUM;
+    hint_state= true;
+    set_also_on_table= true;
+    break;
+  case TokenID::keyword_NO_INDEX_MERGE:
+    hint_type= INDEX_MERGE_HINT_ENUM;
+    hint_state= false;
+    set_also_on_table= true;
     break;
   default:
     DBUG_ASSERT(0);
@@ -502,14 +535,16 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
   if (!tab)
     return false;
 
-  if (is_empty())  // Table level hint
+  if (is_empty() || set_also_on_table)  // Table level hint
   {
     if (tab->set_switch(hint_state, hint_type, false))
     {
       print_warn(pc->thd, ER_WARN_CONFLICTING_HINT, hint_type, hint_state,
                  &qb_name_sys, &table_name_sys, nullptr, nullptr);
     }
-    return false;
+
+    if (is_empty())
+      return false;
   }
 
   for (const Hint_param_index &index_name : *this)
@@ -523,7 +558,7 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
       tab->register_child(idx);
     }
 
-    if (idx->set_switch(hint_state, hint_type, true))
+    if (idx->set_switch(hint_state, hint_type, !set_also_on_table))
     {
       print_warn(pc->thd, ER_WARN_CONFLICTING_HINT, hint_type, hint_state,
                  &qb_name_sys, &table_name_sys, &index_name_sys, nullptr);

sql/opt_hints_parser.h
@@ -51,6 +51,7 @@ enum opt_hints_enum
   DERIVED_CONDITION_PUSHDOWN_HINT_ENUM,
   MERGE_HINT_ENUM,
   SPLIT_MATERIALIZED_HINT_ENUM,
+  INDEX_MERGE_HINT_ENUM,
   MAX_HINT_ENUM // This one must be the last in the list
 };
 
@@ -121,7 +122,9 @@ public:
     keyword_MERGE,
     keyword_NO_MERGE,
     keyword_SPLIT_MATERIALIZED,
-    keyword_NO_SPLIT_MATERIALIZED
+    keyword_NO_SPLIT_MATERIALIZED,
+    keyword_INDEX_MERGE,
+    keyword_NO_INDEX_MERGE
   };
 
   class Token: public Lex_cstring
@@ -398,7 +401,9 @@ private:
       return id == TokenID::keyword_MRR ||
              id == TokenID::keyword_NO_RANGE_OPTIMIZATION ||
              id == TokenID::keyword_NO_ICP ||
-             id == TokenID::keyword_NO_MRR;
+             id == TokenID::keyword_NO_MRR ||
+             id == TokenID::keyword_INDEX_MERGE ||
+             id == TokenID::keyword_NO_INDEX_MERGE;
     }
   };
   class Index_level_hint_type: public TokenChoice<Parser,

ltzhang/mariadb_server

sql/opt_hints.cc
@@ -52,6 +52,7 @@ struct st_opt_hint_info opt_hint_info[]=
   {{STRING_WITH_LEN("JOIN_INDEX")}, false, true, false},
   {{STRING_WITH_LEN("GROUP_INDEX")}, false, true, false},
   {{STRING_WITH_LEN("ORDER_INDEX")}, false, true, false},
+  {{STRING_WITH_LEN("INDEX_MERGE")}, false, false, false},
   {null_clex_str, 0, 0, 0}
 };
 
@@ -565,10 +566,7 @@ bool Opt_hints_table::fix_key_hints(TABLE *table)
     {
       if (key_info->name.streq((*hint)->get_name()))
       {
-        (*hint)->set_fixed();
-        keyinfo_array[j]= static_cast<Opt_hints_key *>(*hint);
-        incr_fully_fixed_children();
-        set_compound_key_hint_map(*hint, j);
+        set_index_hint(*hint, j);
         break;
       }
     }
@@ -810,6 +808,26 @@ void Opt_hints_table::append_hint_arguments(THD *thd, opt_hints_enum hint,
   }
 }
 
+// See comment in header file.
+void Opt_hints_table::set_index_hint(Opt_hints *hint, uint arg)
+{
+  hint->set_fixed();
+  keyinfo_array[arg]= static_cast<Opt_hints_key *>(hint);
+  incr_fully_fixed_children();
+
+  /*
+    Update the index_merge_map to note that the key
+    is referenced by a [NO_]INDEX_HINT associated with
+    the table.
+  */
+  if (hint->is_specified(INDEX_MERGE_HINT_ENUM))
+    index_merge_map.set_key(arg);
+
+  set_compound_key_hint_map(hint, arg);
+
+  // In the future, other hint types can be managed here.
+}
+
 
 /**
   Function returns hint value depending on
@@ -905,10 +923,150 @@ hint_state hint_table_state(const THD *thd,
 }
 
 
-/* 
+/*
+  Inspects the table and corresponding index_merge_map to
+  interpret index merge hint state.
+
+  @param table          The table indicated by the hint
+  @param keyno          The particular key for the index hint
+  @param has_key_hint   [OUT] true if the hint is specified for keyno
+  @param other_key_hint [OUT] true if the hint is not specified for
+                        keyno but is specified for some other key on
+                        the table
+  @param has_table_hint [OUT] true if the hint is not specified for any
+                        specific key, but is specified for the table
+  @parma hint_value     [OUT] true if the hint is specified and enabled
+                        or false if: (1) the hint is specified and false
+                        or (2) the hint is not specified (in the case of
+                        (2), has_key_hint, other_key_hint, and
+                        has_table_hint will be false).
+*/
+static void index_merge_hint_impl(const TABLE *table,
+                                  uint keyno,
+                                  bool &has_key_hint,
+                                  bool &other_key_hint,
+                                  bool &has_table_hint,
+                                  bool &hint_value)
+{
+  Opt_hints_table *table_hints= table->pos_in_table_list->opt_hints_table;
+  has_key_hint= false;
+  other_key_hint= false;
+  has_table_hint= false;
+  hint_value= false;
+
+  // Parent should always be initialized
+  if (!table_hints || keyno == MAX_KEY)
+    return;
+
+  const opt_hints_enum type_arg= INDEX_MERGE_HINT_ENUM;
+
+  // Get the hint state for the specific key, if named.
+  if (table_hints->keyinfo_array.size() > 0 &&
+      table_hints->keyinfo_array[keyno] &&
+      table_hints->keyinfo_array[keyno]->is_specified(type_arg))
+  {
+    has_key_hint= true;
+    hint_value= table_hints->keyinfo_array[keyno]->get_switch(type_arg);
+    return;
+  }
+
+  /*
+    The passed keyno doesn't have the hint specified, but see if another
+    has the [NO_]INDEX_MERGE hint specified.  If not, then see if the table
+    as a whole has the hint specified (implying all keys are affected).
+    There can't be a mix of NO_INDEX_MERGE and INDEX_MERGE hints for the
+    same table, so inspecting the first other specified key is enough.
+  */
+  const uint other_keyno= table_hints->index_merge_map.get_first_keyno();
+  if (table_hints->index_merge_map.has_key_specified() &&
+      table_hints->keyinfo_array[other_keyno] &&
+      table_hints->keyinfo_array[other_keyno]->is_specified(type_arg))
+  {
+    other_key_hint= true;
+    hint_value= table_hints->keyinfo_array[other_keyno]->get_switch(type_arg);
+    return;
+  }
+
+  // No specific key named, see if the table has the hint specified.
+  if (table_hints->is_specified(type_arg))
+  {
+    has_table_hint= true;
+    hint_value= table_hints->get_switch(type_arg);
+  }
+}
+
+
+// See comment in header file.
+index_merge_behavior index_merge_hint(const TABLE *table,
+                                      uint keyno,
+                                      bool *force_index_merge,
+                                      bool *use_cheapest_index_merge)
+{
+  bool has_key_hint= false,
+       other_has_hint= false,
+       has_table_hint= false,
+       hint_value= false;
+
+  index_merge_hint_impl(table,
+                        keyno,
+                        has_key_hint,
+                        other_has_hint,
+                        has_table_hint,
+                        hint_value);
+
+  if (has_key_hint && hint_value)
+  {
+    // Index merge is allowed for this key, so use it.
+    *force_index_merge= true;
+    return index_merge_behavior::USE_KEY;
+  }
+
+  if (other_has_hint && hint_value)
+    // keyno isn't the one with the hint, another key on the table has the hint.
+    return index_merge_behavior::SKIP_KEY;
+
+  if (has_key_hint && !hint_value)
+    // This key is not allowed, so skip it.
+    return index_merge_behavior::SKIP_KEY;
+
+  if (other_has_hint && !hint_value)
+    // Another key is disallowed by the hint, this key is allowed.
+    return index_merge_behavior::USE_KEY;
+
+  if (has_table_hint && hint_value)
+  {
+    // No specific keys mentioned in the hint, so all are implied for the table.
+    *force_index_merge= true;
+    *use_cheapest_index_merge= true;
+    return index_merge_behavior::TABLE_ENABLED;
+  }
+
+  if (has_table_hint && !hint_value)
+    // Merging is disabled for all keys on the table.
+    return index_merge_behavior::TABLE_DISABLED;
+
+  // No hint specified for the table.
+  return index_merge_behavior::NO_HINT;
+}
+
+
+// See comment in header file.
+index_merge_behavior index_merge_hint(const TABLE *table,
+                                      uint keyno)
+{
+  bool force_index_merge_IGNORED= false,
+       use_cheapest_index_merge_IGNORED= false;
+  return index_merge_hint(table,
+                          keyno,
+                          &force_index_merge_IGNORED,
+                          &use_cheapest_index_merge_IGNORED);
+}
+
+
+/*
   @brief
     Check whether a given optimization is enabled for table.keyno.
-  
+
   @detail
     First check if a hint is present, then check optimizer_switch
 */

sql/opt_hints_parser.cc
@@ -128,6 +128,8 @@ Optimizer_hint_tokenizer::find_keyword(const LEX_CSTRING &str)
       return TokenID::keyword_ORDER_INDEX;
     if ("GROUP_INDEX"_Lex_ident_column.streq(str))
       return TokenID::keyword_GROUP_INDEX;
+    if ("INDEX_MERGE"_Lex_ident_column.streq(str))
+      return TokenID::keyword_INDEX_MERGE;
     break;
 
   case 13:
@@ -140,6 +142,8 @@ Optimizer_hint_tokenizer::find_keyword(const LEX_CSTRING &str)
       return TokenID::keyword_NO_ORDER_INDEX;
     if ("NO_GROUP_INDEX"_Lex_ident_column.streq(str))
       return TokenID::keyword_NO_GROUP_INDEX;
+    if ("NO_INDEX_MERGE"_Lex_ident_column.streq(str))
+      return TokenID::keyword_NO_INDEX_MERGE;
     break;
 
   case 15:
@@ -517,6 +521,22 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
   opt_hints_enum hint_type;
   bool hint_state; // ON or OFF
 
+  /*
+    Intended for the [NO_]INDEX_MERGE hints originally, this is set
+    true when the hint should be set also on the table as well as
+    the index.  However, for such a hint set on the table, the check_parent flag
+    is inverted from this value; that is, if set_also_on_table is
+    true, then check_parent will be false (see below).  This is
+    because the presence of the hint on the table is treated differently
+    than the hint applying to all keys on the table.  For example,
+    INDEX_MERGE(table) means that the hint applies to all keys while
+    INDEX_MERGE(table, k1) means that the hint applies to key k1.  In
+    both cases, however, we set the hint both as an index-level hint and
+    as a table-level hint, but when inspecting it during index_merge_hint,
+    it is given special treatment (see that function for details).
+  */
+  bool set_also_on_table= false;
+
   switch (index_level_hint_type.id())
   {
   case TokenID::keyword_NO_ICP:
@@ -567,6 +587,16 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
     hint_type= GROUP_INDEX_HINT_ENUM;
     hint_state= false;
     break;
+  case TokenID::keyword_INDEX_MERGE:
+    hint_type= INDEX_MERGE_HINT_ENUM;
+    hint_state= true;
+    set_also_on_table= true;
+    break;
+  case TokenID::keyword_NO_INDEX_MERGE:
+    hint_type= INDEX_MERGE_HINT_ENUM;
+    hint_state= false;
+    set_also_on_table= true;
+    break;
   default:
     DBUG_ASSERT(0);
     return true;
@@ -594,7 +624,7 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
     Otherwise this is a group of index-level hints:
     NO_INDEX(t1 idx1, idx2) NO_ICP(t2 idx_a, idx_b, idx_c)
   */
-  if (is_empty())
+  if (is_empty() || set_also_on_table)  // Table level hint
   {
     uint warn_code= 0;
     if (is_compound_hint(hint_type) &&
@@ -616,7 +646,9 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
     {
       tab->get_key_hint_bitmap(hint_type)->parsed_hint= this;
     }
-    return false;
+
+    if (is_empty())
+      return false;
   }
 
   // Key names for a compound hint are first collected into the array:
@@ -638,7 +670,7 @@ bool Parser::Index_level_hint::resolve(Parse_context *pc) const
 
     if (!is_compound_hint(hint_type))
     {
-      if (key->set_switch(hint_state, hint_type, true))
+      if (key->set_switch(hint_state, hint_type, !set_also_on_table))
       {
         print_warn(pc->thd, ER_WARN_CONFLICTING_INDEX_HINT_FOR_KEY,
                    hint_type, hint_state, &qb_name_sys, &table_name_sys,

sql/opt_hints_parser.h
@@ -55,6 +55,7 @@ enum opt_hints_enum
   JOIN_INDEX_HINT_ENUM,
   GROUP_INDEX_HINT_ENUM,
   ORDER_INDEX_HINT_ENUM,
+  INDEX_MERGE_HINT_ENUM,
   MAX_HINT_ENUM // This one must be the last in the list
 };
 
@@ -134,7 +135,9 @@ public:
     keyword_GROUP_INDEX,
     keyword_NO_GROUP_INDEX,
     keyword_ORDER_INDEX,
-    keyword_NO_ORDER_INDEX
+    keyword_NO_ORDER_INDEX,
+    keyword_INDEX_MERGE,
+    keyword_NO_INDEX_MERGE
   };
 
   class Token: public Lex_cstring
@@ -423,7 +426,9 @@ private:
              id == TokenID::keyword_ORDER_INDEX ||
              id == TokenID::keyword_NO_ORDER_INDEX ||
              id == TokenID::keyword_GROUP_INDEX ||
-             id == TokenID::keyword_NO_GROUP_INDEX;
+             id == TokenID::keyword_NO_GROUP_INDEX ||
+             id == TokenID::keyword_INDEX_MERGE ||
+             id == TokenID::keyword_NO_INDEX_MERGE;
     }
   };
   class Index_level_hint_type: public TokenChoice<Parser,